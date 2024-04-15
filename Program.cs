d24743f003323e78512942ed2cf6a1995b3a320a
Jayabharathi3 <bharathibhuvana10@gmail.com>
2024-04-15 20:56:24 +0530

    updated successfully

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
 ﻿using System;
-public class Pattern
+namespace jagged
 {
-    public static void Main()
+    public class Pattern
     {
-      // type the code here
+        public static void Main()
+        {
+            int[][] jaggedArray = new int[4][];
+            jaggedArray[0] = new int[3];
+            jaggedArray[1] = new int[5];
+            jaggedArray[2] = new int[6];
+            jaggedArray[3] = new int[4];
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = i * j + 60;
+                    Console.WriteLine("CPU usage of {0} node is {1}%", i + 1, jaggedArray[i][j]);
+
+                }
+            }
+        }
     }
+
 }
+
